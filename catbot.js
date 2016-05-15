9599a8909c18f42a30d21e52ca8e835d0bc30f79
Moishe Lettvin <moishel@gmail.com>
2016-05-14 18:17:17 -0600

    Listen on HTTP port

--- catbot.js
+++ catbot.js
@@ -1,11 +1,33 @@
-/**
- * Example for creating and working with the Slack RTM API.
- */
-
-/* eslint no-console:0 */
-
 catbotModule = require('./catbot-runner');
 catbotRunner = new catbotModule.CatRunner();
 
 catbotRunner.init();
 catbotRunner.start();
+
+// In the future we should serve a pretty webpage with user stats!
+// For now we have a simple app that doesn't do anything but listen
+// and serve a dummy webpage
+
+const express = require('express')
+const proxy = require('express-http-proxy')
+const bodyParser = require('body-parser')
+const _ = require('lodash')
+
+var app = express()
+
+if (process.env.PROXY_URI) {
+  app.use(process.env.PROXY_URI), {
+    forwardPath: function(req, res) { return require('url').parse(req.url).path }
+  }
+}
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', function(req, res) { res.send('\n � � \n') })
+
+app.listen(process.env.PORT, function(err) {
+  if (err) throw err
+
+  console.log('Listening on ' + process.env.PORT);
+})
