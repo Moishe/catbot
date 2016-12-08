5ad308cdf162d070439e70396bbd7162aa328aba
Moishe Lettvin <mlettvin@slack-corp.com>
2016-12-08 14:58:34 -0800

    Remove hello demo

--- modules/hello.js
+++
@@ -1,7 +0,0 @@
-exports.handle = function(pieces, storageFactory, callback) {
-	if (pieces.length){
-		callback({'message': 'Hello ' + pieces[0]});
-	}else{
-		callback({'message': 'Hello world.'});
-	}
-}
