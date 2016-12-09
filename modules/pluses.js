2c47e1e71d3aed4d4d07f448054fb394edf60cbc
Monica Wilkinson <mwilkinson@slack-corp.com>
2016-12-08 16:25:55 -0800

    fix storing messages

--- modules/pluses.js
+++ modules/pluses.js
@@ -7,13 +7,15 @@ exports.handle = function(sender, pieces, storageFactory, callback) {
 		userString = "<@" + sender + ">";
 	}
 
-	userStorage.getItem('pluses', function(pluses){
+	var message = null;
+	var userStorage.getItem('pluses', function(pluses){
 		if (!pluses){
 			message = "Alas, " + userString + " has no pluses. You should give them some!";
 		}else{
 			message = userString + " has " + pluses + " pluses!";
 		}
 
-		callback({'message': message});
 	});
+
+	callback({'message': message});
 }
