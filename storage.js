cff10eaba8754689546c45ac4390b08582734c2f
Monica Wilkinson <mwilkinson@slack-corp.com>
2016-12-09 06:09:40 -0800

    Log results

--- storage.js
+++ storage.js
@@ -7,10 +7,11 @@ function Storage(connection, table, id) {
 Storage.prototype.getItem = function(key, callback) {
 	var sql = "SELECT data_value FROM " + this.table + " WHERE id=? AND data_key=?";
 	this.connection.query(sql, [this.id, key], function(err, results){
+        console.log('Results for ' + key + ' are:');
+        console.dir(results);
 		if (results && results.length > 0){
 			callback(results[0]['data_value']);
 		} else{
-			console.log("Error getting results")
 			callback(null);
 		}
 	});
