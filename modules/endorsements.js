aeb1ab59920a52482e5613aeb32d5d942bc8ced4
Moishe Lettvin <mlettvin@slack-corp.com>
2016-12-08 17:04:18 -0800

    Update endorsements bots to use new storage model

--- modules/endorsements.js
+++ modules/endorsements.js
@@ -1,26 +1,28 @@
 exports.handle = function(sender, pieces, storageFactory, callback) {
   var user = pieces.shift();
 
-  var endorsements = JSON.parse(userStorage.getItem('endorsements') || '{}');
+  var userStorage = storageFactory.getUserStorage(user);
+  userStorage.getItem('endorsements', function(endorsements){
+    endorsements = JSON.parse(endorsements || '{}'); 
+    var annotatedEndorsements = [];
 
-  var annotatedEndorsements = [];
-
-  for (var endorsement in endorsements) {
-    if (endorsements[endorsement] == 1) {
-      annotatedEndorsements.push(endorsement);
-    } else {
-      annotatedEndorsements.push(endorsement + " (" + endorsements[endorsement] + ")");
+    for (var endorsement in endorsements) {
+      if (endorsements[endorsement] == 1) {
+        annotatedEndorsements.push(endorsement);
+      } else {
+        annotatedEndorsements.push(endorsement + " (" + endorsements[endorsement] + ")");
+      }
     }
-  }
 
-  var message = '';
-  if (Object.keys(endorsements).length) {
-    message = '*' + user + '* has been endorsed for: ' + annotatedEndorsements.join(', ');
-  } else {
-    message = '*' + user + '* has no endorsements! You should endorse them for something!';
-  }
+    var message = '';
+    if (Object.keys(endorsements).length) {
+      message = '*' + user + '* has been endorsed for: ' + annotatedEndorsements.join(', ');
+    } else {
+      message = '*' + user + '* has no endorsements! You should endorse them for something!';
+    }
 
-  callback({
-    'message': message
+    callback({
+      'message': message
+    });    
   });
 }
