7465b1a6a4e06ed2497c5e6bdc1b8e5d9ca0e1bc
Monica Wilkinson <mwilkinson@slack-corp.com>
2016-12-08 18:33:59 -0800

    only the env var

--- catbot-runner.js
+++ catbot-runner.js
@@ -26,7 +26,7 @@ CatRunner.prototype.init = function(client, events, tok) {
 	var config = require('config');
 	var mysql = require('mysql');
 	var dbConfig = config.get('DB');
-	this.connection = mysql.createConnection(dbConfig);
+	this.connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
 	this.connection.connect();
 
 	// Ensure tables exist.
