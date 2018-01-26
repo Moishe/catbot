8805f468744b16bb949d196aa8452789466f82ee
Ryan Young <ryoung786@gmail.com>
2018-01-26 18:07:11 -0500

    ?911 opsgenie

---
+++ modules/911.js
@@ -0,0 +1,25 @@
+var opsgenie = require("opsgenie-sdk");
+
+exports.handle = function(sender, pieces, db, callback) {
+  const msg = pieces.slice(1).join(" ");
+
+  opsgenie.configure({
+    api_key: "efeab72c-f474-47e5-9d57-2f42b2536036"
+  });
+
+  opsgenie.alertV2.create(
+    {
+      message: msg,
+      user: sender.profile.display_name
+    },
+    function(error, alert) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log("Create Alert Response: ");
+        console.log(alert);
+        callback({ message: "OK, engineers have been paged via OpsGenie!" });
+      }
+    }
+  );
+};
